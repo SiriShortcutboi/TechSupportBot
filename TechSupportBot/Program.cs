5db95b044b309f2ebd6014436d068f851cbf4bea
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-20 15:29:40 -0700

    initial work from last night

---
+++ TechSupportBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+					
+public class Program
+{
+    static string devicePower = "default";
+    static string deviceError = "default";
+    static string deviceWifi = "default";
+
+    static bool powerStatus = false;
+    static bool errorStatus = false;
+    static bool wifiStatus = false;
+	public static void Main()
+	{
+		Console.WriteLine("Welcome to Tech Support, I'm going to ask you a few questions.\n" + 
+        "Press Enter to start");
+        Console.ReadKey();
+
+        Console.WriteLine("Is the device turning on?");
+        devicePower = Console.ReadLine();
+
+        Console.WriteLine("Is there an error message?");
+        deviceError =Console.ReadLine();
+
+        Console.WriteLine("Is it connected to Wi-Fi?");
+        deviceWifi = Console.ReadLine();
+        if (devicePower.Trim().ToLower == "yes")
+        {
+            powerStatus = true;
+
+        }
+        else if(devicePower.Trim().ToLower == "no")
+        {
+            powerStatus = false;
+        }
+        else
+        {
+            powerStatus = false;
+        }
+
+
+
+	}
+
+
+
+   
+
+    void ShowSupportMessage(string issue, bool urgent = false, string technician = "General Help")
+    {
+        
+
+    }
+
+}
